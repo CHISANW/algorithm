48e843732826b3e587ec005b26b669dcd1577b23
CHISANW <keuye06380618@gmail.com>
2023-08-23 17:37:10 +0900

    변경

---
+++ src/test/test2.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test2 {
+    public static void main(String[] args) {
+        int i  = 1;
+
+        for(int n =0; n<10; n++){
+            System.out.println("n = "+n);
+        }
+    }
+}
