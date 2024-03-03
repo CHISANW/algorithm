85c3ded4c5d169f03d98cc3eac91b34772246e67
CHISANW <keuye06380618@gmail.com>
2024-03-04 00:38:53 +0900

    일반수학1 7/6 풀이

--- src/BaekJoonAlgorithm/일반수학/세탁소사장동혁.java
+++ src/BaekJoonAlgorithm/일반수학/세탁소사장동혁.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 문제번호 2720번
+ */
 public class 세탁소사장동혁 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
