aea3f5306aa6a01e65de03c3e5558ceea417f5bf
CHISANW <keuye06380618@gmail.com>
2024-03-08 03:00:44 +0900

    기하:직사각형과 삼각형 2/8 완료

---
+++ src/BaekJoonAlgorithm/기하직사각형과삼각형/직사각형.java
@@ -0,0 +1,17 @@
+package BaekJoonAlgorithm.기하직사각형과삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 문제번호 직사각형 27323번
+ */
+public class 직사각형 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a*b);
+    }
+}
