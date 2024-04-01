f24096fe618a825a85cb689b1421cff574be91bf
CHISANW <keuye06380618@gmail.com>
2024-04-01 23:34:23 +0900

    큐 - 요세푸수스0 (11866번) 풀이

---
+++ src/BaekJoonAlgorithm/스택큐덱/요세푸스문제0.java
@@ -0,0 +1,48 @@
+package BaekJoonAlgorithm.스택큐덱;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 문제번호 11866 큐를 이용한 요새푸스스 문제
+ */
+public class 요세푸스문제0 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] result = new int[n];
+        int j = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1;i<=n;i++ ){
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()){
+            for (int i =1;i<=k;i++){
+                if (i==k){
+                    Integer poll = queue.poll();
+                    result[j] = poll;
+                    j++;
+                }
+                else {
+                    Integer poll = queue.poll();
+                    queue.add(poll);
+                }
+            }
+        }
+        System.out.print("<");
+        for (int i = 0; i < result.length - 1; i++) {
+            System.out.print(result[i] + ", ");
+        }
+        System.out.print(result[result.length - 1] + ">");
+    }
+}
