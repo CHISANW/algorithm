2de9d16431525f5d3f487ce8e18d2bdb3ee0e8c0
CHISANW <keuye06380618@gmail.com>
2024-04-25 04:39:53 +0900

    REDEME 추가

---
+++ src/test.java
@@ -0,0 +1,20 @@
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//import java.util.Scanner;
+//import java.util.StringTokenizer;
+//
+//public class test {
+//    public static void main(String[] args) throws IOException {
+//
+//        Scanner stdIn = new Scanner(System.in);
+//
+//        int n = stdIn.nextInt();
+//
+//        for (int i=n;i>;i++){
+//
+//        }
+//
+//    }
+//}
