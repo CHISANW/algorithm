5b5b396ea9c7a4864ae4125de0220669660426d3
CHISANW <keuye06380618@gmail.com>
2024-05-14 23:28:18 +0900

    2024-05-14 [백준] - 알파벳(골드4)

---
+++ src/BaekJoonAlgorithm/재귀호출/백트래킹/알파벳.java
@@ -0,0 +1,55 @@
+package BaekJoonAlgorithm.재귀호출.백트래킹;
+
+import java.util.*;
+
+public class 알파벳 {
+    static int[][] board;
+    static int r ,c ;
+    static boolean[] check = new boolean[26];
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        r = sc.nextInt(); // row
+        c = sc.nextInt(); // col
+
+        board = new int[r][c];
+        for(int i =0;i<r;i++){
+            String line = sc.next();
+            for(int j =0;j<c;j++){
+                board[i][j] = line.charAt(j)-'A';       // 술자로 변경
+            }
+        }
+
+        check[board[0][0]] = true;
+        System.out.println(solve(0,0));
+    }
+
+
+    static int[] dr = {-1,0,1,0};
+    static int[] dc = {0,1,0,-1};
+    static int solve(int row, int col){
+        int result = 0;
+        for(int i =0;i<4;i++){
+            int nextRow = row+dr[i] ,nextCol =col + dc[i];
+            if(isBoundIndex(nextRow,nextCol, r, c)) continue;
+            int next = board[nextRow][nextCol];
+            if(check[next]) continue;
+
+            check[next]= true;
+            int nextResult = solve(nextRow,nextCol);
+            result = Math.max(result,nextResult);
+            check[next] = false;
+        }
+
+        return result + 1;
+
+    }
+
+    static boolean isBoundIndex(int nextR, int nextC, int row, int col){
+        return nextR < 0 || nextC < 0 || nextR >= row || nextC >= col;
+    }
+
+
+
+}
