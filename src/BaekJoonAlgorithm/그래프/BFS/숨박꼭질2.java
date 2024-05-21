b24bb14377b47e0963096a15d095692ef64c08f5
CHISANW <keuye06380618@gmail.com>
2024-05-22 05:56:26 +0900

    2024-05-22 [백준] - 12851_숨박꼭질2(Gold_4)

---
+++ src/BaekJoonAlgorithm/그래프/BFS/숨박꼭질2.java
@@ -0,0 +1,43 @@
+package BaekJoonAlgorithm.그래프.BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 숨박꼭질2 {
+    static int[] visited = new int[100001];
+    static int[] count = new int[100000];
+    static int n, k;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        visited[n] = 1;
+        count[n] = 1;
+        while(!q.isEmpty()){
+            int now = q.poll();
+            if(now == k) break;
+            int[] next = {now - 1, now + 1, 2*now};
+            for(int i=0;i<3;i++){
+                if(!isRange(next[i])) continue;
+                if(visited[next[i]] == 0){
+                    visited[next[i]] = visited[now] + 1;
+                    count[next[i]] = count[now];
+                    q.add(next[i]);
+                }else if(visited[next[i]] == visited[now]+1){
+                    count[next[i]]+=count[now];
+                }
+            }
+        }
+        System.out.println(visited[k]-1);
+        System.out.println(count[k]);
+
+    }
+    static boolean isRange(int x){
+        return x>=0 && x<=100000;
+    }
+}
