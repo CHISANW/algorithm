408ece4bdc8eb578c98a234759f15a33bc513459
CHISANW <keuye06380618@gmail.com>
2024-05-27 05:24:35 +0900

    2024-05-27 [백준] 해시 - 11652_카드(Silver_4)

---
+++ src/BaekJoonAlgorithm/해시/카드.java
@@ -0,0 +1,30 @@
+package BaekJoonAlgorithm.해시;
+
+import java.util.*;
+import java.io.*;
+
+public class 카드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<Long, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            long m = Long.parseLong(br.readLine());
+            map.put(m, map.getOrDefault(m, 0) + 1);
+        }
+
+        int max = -1;
+        long num = 0;
+
+        for (long l : map.keySet()) {
+            if (map.get(l) > max) {
+                max = map.get(l);
+                num = l;
+            } else if (map.get(l) == max) {
+                num = Math.min(l, num);
+            }
+        }
+        System.out.println(num);
+    }
+}
