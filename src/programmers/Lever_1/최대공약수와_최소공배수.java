2be3d3a6bbfa852f647b3e088e2b3343b6452ca9
CHISANW <keuye06380618@gmail.com>
2024-06-05 00:11:58 +0900

    2024-06-05 [프로그래머스] Level-1 - 최대공약수와 최소공배수

---
+++ src/programmers/Lever_1/최대공약수와_최소공배수.java
@@ -0,0 +1,29 @@
+package programmers.Lever_1;
+
+import java.util.Arrays;
+
+public class 최대공약수와_최소공배수 {
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 12;
+        System.out.println(Arrays.toString(solution(n,m)));     //result => [3, 12]
+    }
+
+    public static int[] solution(int n, int m) {
+        int[] answer = new int[2];
+
+        int a = n;
+        int b = m;
+        while(b!=0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        answer[0] = a;
+        answer[1] = (n * m)/a;
+        System.out.println(a);
+
+        return answer;
+    }
+}
