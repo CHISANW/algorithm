aaad83b0da7f770b75bc32b5f567e765da5fc44d
CHISANW <keuye06380618@gmail.com>
2024-06-07 23:06:22 +0900

    2024-06-07 [프로그래머스] - 가장 가까운 같은 글자(Level_1)

---
+++ src/programmers/Lever_1/가장_가까운_같은_글자.java
@@ -0,0 +1,33 @@
+package programmers.Lever_1;
+
+import java.util.Arrays;
+
+public class 가장_가까운_같은_글자 {
+    public static void main(String[] args) {
+
+        String s = "banana";
+        System.out.println(Arrays.toString(solution(s)));
+    }
+
+    public static int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        for(int i=0;i<s.length();i++){
+            if(i !=0){
+                int idx = s.substring(0,i).lastIndexOf(s.charAt(i));
+                if(idx != -1){
+                    answer[i] = i-idx;
+
+                }
+                else{
+                    answer[i] = idx;
+                }
+            }
+            else{
+                answer[i] = -1;
+            }
+        }
+
+        return answer;
+    }
+}
