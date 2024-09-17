6e359be94c616eed97827b506aefc1eb57671035
CHISANW <keuye06380618@gmail.com>
2024-09-18 03:07:22 +0900

    2024-09-18[백준] - 떡 먹는 호랑이(Silver_1)

---
+++ src/BaekJoonAlgorithm/떡먹는호랑이.java
@@ -0,0 +1,35 @@
+package BaekJoonAlgorithm;
+
+import java.util.*;
+
+public class 떡먹는호랑이 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int k = sc.nextInt();
+        int[] dp = new int[d+1];
+
+        int a = 0;
+        int b= 0;
+        boolean find = false;
+        for (int i =1 ; i<=k;i++){
+            if (find) break;
+            for (int j =1; j<=k;j++){
+                dp[1] = i;
+                dp[2] = j;
+                for (int value = 3; value<=d;value++){
+                    dp[value] = dp[value-1] + dp[value-2];
+                }
+                if (dp[d] == k){
+                    a = i;
+                    b = j;
+                    find = true;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
