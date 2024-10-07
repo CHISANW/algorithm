249e6b34952f0076098d1e3e4eb445d6e596b8c7
CHISANW <keuye06380618@gmail.com>
2024-10-08 02:12:24 +0900

    2024-10-08[백준] - 주지수 (Silver_1)

---
+++ src/BaekJoonAlgorithm/구간합/주지수.java
@@ -0,0 +1,38 @@
+package BaekJoonAlgorithm.구간합;
+
+import java.io.*;
+import java.util.*;
+
+public class 주지수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        long[][] arr = new long[n + 1][m + 1];
+        long[][] sum = new long[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+        for (int i = 0;i<k;i++) {
+            st = new StringTokenizer(br.readLine());
+            int r1 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+            long ans = sum[r2][c2] - sum[r1 - 1][c2] - sum[r2][c1 - 1] + sum[r1 - 1][c1 - 1];
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
