9a3331c1ada81dbcb3c5d7d14f36aef08d3b25c5
CHISANW <keuye06380618@gmail.com>
2024-11-06 05:09:40 +0900

    2024-11-06[백준] - BaekJoon_17216(Silver_2)
    - 가장 큰 감소 부분 수열
    - 동적 계획법 알고리즘 풀이

---
+++ src/BaekJoonAlgorithm/동적계획법/BaekJoon_17216.java
@@ -0,0 +1,32 @@
+package BaekJoonAlgorithm.동적계획법;
+
+import java.util.Scanner;
+
+public class BaekJoon_17216 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i =0 ; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int maxValue =0 ;
+
+        for (int i = 0; i<n;i++){
+            dp[i] = arr[i];
+
+            for (int  j = 0; j<i;j++){
+                if (arr[i] < arr[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+arr[i]);
+                }
+            }
+
+            maxValue = Math.max(dp[i] ,maxValue);
+        }
+        System.out.println(maxValue);
+    }
+}
