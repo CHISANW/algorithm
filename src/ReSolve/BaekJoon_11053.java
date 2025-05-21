2f6bfbc3068931b678b11d8b9fdf01ac4c9b5899
CHISANW <keuye06380618@gmail.com>
2025-05-21 23:25:25 +0900

    2025-05-21[백준] - BaekJoon_11053(Silver_2)[재풀이]

---
+++ src/ReSolve/BaekJoon_11053.java
@@ -0,0 +1,29 @@
+package ReSolve;
+
+import java.util.Scanner;
+
+public class BaekJoon_11053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n];
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        System.out.println(maxLength);
+    }
+}
