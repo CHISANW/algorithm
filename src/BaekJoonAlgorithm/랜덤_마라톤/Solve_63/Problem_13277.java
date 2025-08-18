e95b1af3d3a23af4476cec85222cffcfecf287b9
CHISANW <keuye06380618@gmail.com>
2025-08-18 23:47:34 +0900

    [Java] 백준 랜덤 마라톤_63 - 큰 수 곱셈(13277)

---
+++ src/BaekJoonAlgorithm/랜덤_마라톤/Solve_63/Problem_13277.java
@@ -0,0 +1,13 @@
+package BaekJoonAlgorithm.랜덤_마라톤.Solve_63;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Problem_13277 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.multiply(b));
+    }
+}
