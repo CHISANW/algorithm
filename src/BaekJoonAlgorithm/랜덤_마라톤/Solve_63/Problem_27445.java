c32df0ee7f947d304ef048efe8c912c817a71014
CHISANW <keuye06380618@gmail.com>
2025-08-19 01:49:01 +0900

    [Java] 백준 랜덤 마라톤_63 - 위치 계산(27445)

---
+++ src/BaekJoonAlgorithm/랜덤_마라톤/Solve_63/Problem_27445.java
@@ -0,0 +1,50 @@
+package BaekJoonAlgorithm.랜덤_마라톤.Solve_63;
+
+import java.io.*;
+
+public class Problem_27445 {
+    static Node[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), m = read();
+        arr = new Node[n + m - 1];
+
+        int i = 0;
+        for (int j = 1; j <= n; j++) arr[i++] = new Node(j, 1, read());
+        for (int j = 2; j <= m; j++) arr[i++] = new Node(n, j, read());
+
+        bw.write(search(n, m));
+        bw.flush();
+    }
+
+    private static String search(int n, int m) {
+        for (int i = 1; i <= n; i++) for (int j = 1; j <= m; j++) if (measure(i, j)) return i + " " + j;
+
+        return "";
+    }
+
+    private static boolean measure(int y, int x) {
+        for (Node n : arr) if (Math.abs(n.y - y) + Math.abs(n.x - x) != n.w) return false;
+
+        return true;
+    }
+
+    private static class Node {
+        int y, x, w;
+
+        Node(int y, int x, int w) {
+            this.y = y;
+            this.x = x;
+            this.w = w;
+        }
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+}
