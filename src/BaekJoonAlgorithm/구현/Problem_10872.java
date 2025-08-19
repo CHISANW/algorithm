fcd12b06d5cabd99882e3baa65aae2594c5a16f1
CHISANW <keuye06380618@gmail.com>
2025-08-20 02:25:59 +0900

    [Java] 백준 - 팩토리얼(10872)

---
+++ src/BaekJoonAlgorithm/구현/Problem_10872.java
@@ -0,0 +1,16 @@
+package BaekJoonAlgorithm.구현;
+
+import java.util.Scanner;
+
+public class Problem_10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+
+    }
+    static int factorial(int n){
+        if(n==0) return 1;
+        return n*factorial(n-1);
+    }
+}
