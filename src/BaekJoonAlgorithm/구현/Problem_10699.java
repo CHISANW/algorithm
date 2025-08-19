616d33b0414f79bdadcf75f03ca5e446747d7ace
CHISANW <keuye06380618@gmail.com>
2025-08-20 02:22:11 +0900

    [Java] 백준 - 오늘 날짜(10699)

---
+++ src/BaekJoonAlgorithm/구현/Problem_10699.java
@@ -0,0 +1,14 @@
+package BaekJoonAlgorithm.구현;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+
+public class Problem_10699 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(today.format(formatter));
+    }
+}
