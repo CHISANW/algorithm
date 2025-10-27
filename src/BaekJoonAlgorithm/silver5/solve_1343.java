08c3ba3f9b4eeb3b58869dceb29965ce341d70cc
CHISANW <keuye06380618@gmail.com>
2025-10-28 01:40:03 +0900

    [Java] 백준 - 폴리오미노(1343) 로직 간소화 및 코드 최적화

--- src/BaekJoonAlgorithm/silver5/solve_1343.java
+++ src/BaekJoonAlgorithm/silver5/solve_1343.java
@@ -7,38 +7,15 @@ public class solve_1343 {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
 
-        if(!line.contains("X")){
-            System.out.println(line);
+        String ans = "";
+
+        String temp = line.replaceAll("XXXX" , "AAAA");
+        ans = temp.replaceAll("XX", "BB");
+
+        if(ans.contains("X")){
+            System.out.println(-1);
             return;
         }
-
-        StringBuilder sb = new StringBuilder();
-        String[] split = line.split("\\.", -1);
-
-        for(int i =0; i<split.length;i++){
-
-            String s= split[i];
-
-            if(s.length() ==0){
-                sb.append(".");
-                continue;
-            }
-
-            if(s.length()%2 != 0){
-                System.out.println(-1);
-                return;
-            }
-
-            int a = s.length() /4;
-            int b = (s.length()%4)/2;
-
-            sb.append("AAAA".repeat(a));
-            sb.append("BB".repeat(b));
-
-            if(i != split.length-1)
-                sb.append(".");
-
-        }
-        System.out.println(sb.toString());
+        System.out.println(ans);
     }
 }
