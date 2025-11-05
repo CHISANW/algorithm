b5fc38765d7d250155796e6455fcee28c16b6861
CHISANW <keuye06380618@gmail.com>
2025-11-05 22:04:09 +0900

    [Java] 백준 - K번째 수(11004)

---
+++ src/BaekJoonAlgorithm/silver5/solve_11004.java
@@ -0,0 +1,25 @@
+package BaekJoonAlgorithm.silver5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class solve_11004 {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[k-1]);
+
+    }
+}
