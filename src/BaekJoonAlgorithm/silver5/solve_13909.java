145f263f302da36730576aabc81415758caeaf34
CHISANW <keuye06380618@gmail.com>
2025-11-16 17:28:50 +0900

    [Java] 백준 - 창문 닫기(13909)

---
+++ src/BaekJoonAlgorithm/silver5/solve_13909.java
@@ -0,0 +1,11 @@
+package BaekJoonAlgorithm.silver5;
+
+import java.util.Scanner;
+
+public class solve_13909 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println((long) Math.sqrt(n));
+    }
+}
