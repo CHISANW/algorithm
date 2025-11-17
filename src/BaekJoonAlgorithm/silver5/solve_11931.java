38559a62956fe6176580b70fddc05bcdc10ab5c6
CHISANW <keuye06380618@gmail.com>
2025-11-18 01:30:59 +0900

    [Java] 백준 - 수 정렬하기 4(11931)

---
+++ src/BaekJoonAlgorithm/silver5/solve_11931.java
@@ -0,0 +1,31 @@
+package BaekJoonAlgorithm.silver5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class solve_11931 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = n - 1; i >= 0; i--) {
+            bw.write(arr[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
