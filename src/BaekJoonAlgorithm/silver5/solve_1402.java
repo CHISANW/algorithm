e292fdfcf535203e1f2270eb085192cc6e8ee0fb
CHISANW <keuye06380618@gmail.com>
2025-11-27 01:53:39 +0900

    [Java] 백준 - 아무래도이문제는A번난이도인것같다(1402)

---
+++ src/BaekJoonAlgorithm/silver5/solve_1402.java
@@ -0,0 +1,17 @@
+package BaekJoonAlgorithm.silver5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class solve_1402 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0){
+            br.readLine();
+            sb.append("YES\n");
+        }
+        System.out.println(sb);
+    }
+}
