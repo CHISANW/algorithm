5f53972cd979fe8374904e6e918cc65e2bf361e1
CHISANW <keuye06380618@gmail.com>
2026-01-01 23:31:11 +0900

    [Java] 백준 - 수 복원하기(2312)

---
+++ src/BaekJoonAlgorithm/silver4/solve_2312.java
@@ -0,0 +1,30 @@
+package BaekJoonAlgorithm.silver4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class solve_2312 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb  =new StringBuilder();
+        while (t-- > 0){
+            int n = Integer.parseInt(br.readLine());
+            for(int i =2; i<=n;i++){
+                int cnt = 0;
+                while (n % i == 0){
+                    n/=i;
+                    cnt++;
+                }
+                if(cnt !=0){
+                    sb.append(i).append(" ").append(cnt).append("\n");
+                }
+                if( n ==0){
+                    break;
+                }
+            }
+        }
+        System.out.println(sb.toString());
+
+    }
+}
