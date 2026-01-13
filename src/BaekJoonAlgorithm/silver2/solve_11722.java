0b83841308ffe1118676f5f9cd6b2e341445d6d2
CHISANW <keuye06380618@gmail.com>
2026-01-14 01:39:47 +0900

    [Java] 백준 - 가장 긴 감소하는 부분 수열(11722)

---
+++ src/BaekJoonAlgorithm/silver2/solve_11722.java
@@ -0,0 +1,33 @@
+package BaekJoonAlgorithm.silver2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class solve_11722 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i =0; i<n;i++){
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        int ans = 0;
+
+        for(int i =0; i<n;i++){
+            dp[i] = 1;
+            for(int j =0; j<i;j++){
+                if(a[j] > a[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+    }
+}
