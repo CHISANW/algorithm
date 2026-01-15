ca5d015ab6da26e5985b0829e3d817e3255bda5e
CHISANW <keuye06380618@gmail.com>
2026-01-15 23:59:24 +0900

    [Java] 백준 - 이항 계수 2 (11051)

---
+++ src/BaekJoonAlgorithm/silver2/solve_11051.java
@@ -0,0 +1,29 @@
+package BaekJoonAlgorithm.silver2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class solve_11051 {
+    static final int MOD = 10007;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            dp[i][0] = 1;      // nC0 = 1
+            dp[i][i] = 1;      // nCn = 1
+
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % MOD;
+            }
+        }
+        System.out.println(dp[N][K]);
+    }
+
+}
