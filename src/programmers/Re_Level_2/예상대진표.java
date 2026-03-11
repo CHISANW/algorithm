9a4ebf8324a1369489a54b8cfe84d22e3d362b15
CHISANW <keuye06380618@gmail.com>
2026-03-12 00:01:07 +0900

    [Java] 프로그래머스 - 예상대진표

---
+++ src/programmers/Re_Level_2/예상대진표.java
@@ -0,0 +1,18 @@
+package programmers.Re_Level_2;
+
+public class 예상대진표 {
+    public static void main(String[] args) {
+        System.out.println(solution(8,4,7));
+    }
+
+    public static int solution(int n, int a, int b) {
+
+        int answer = 0;
+        while (a != b){
+            a = (a+1)/2;
+            b = (b+1)/2;
+            answer++;
+        }
+        return answer;
+    }
+}
