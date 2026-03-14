d0edb87567a904ffce373657b0207fdc8dbf4482
CHISANW <keuye06380618@gmail.com>
2026-03-15 03:31:24 +0900

    [Java] 프로그래머스 - 타일링

---
+++ src/programmers/Re_Level_2/타일링.java
@@ -0,0 +1,20 @@
+package programmers.Re_Level_2;
+
+public class 타일링 {
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+        System.out.println(solution(4));
+
+    }
+
+    static int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i =3;i<=n;i++){
+            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+        }
+        return dp[n];
+    }
+}
