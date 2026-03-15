403bc19629ac8ef481ef96fbb3426179b46e992a
CHISANW <keuye06380618@gmail.com>
2026-03-16 00:19:06 +0900

    [Java] 프로그래머스 - 가장큰수

---
+++ src/programmers/Re_Level_2/가장큰수.java
@@ -0,0 +1,31 @@
+package programmers.Re_Level_2;
+
+import java.util.*;
+
+public class 가장큰수 {
+    public static void main(String[] args) {
+
+    }
+
+    static String solution(int[] numbers) {
+        List<String> list = new ArrayList<>();
+
+        for(int number : numbers){
+            list.add(String.valueOf(number));
+        }
+
+        list.sort((o1, o2) -> {
+            int a = Integer.parseInt(o1 + o2);
+            int b = Integer.parseInt(o2 + o1);
+
+            return Integer.compare(b,a);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(String i : list){
+            sb.append(i);
+        }
+        return sb.charAt(0) == '0'? "0" : sb.toString();
+
+    }
+}
