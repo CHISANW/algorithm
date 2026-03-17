750e8c98db4d9fcce6019e225aa5c6c772589f53
CHISANW <keuye06380618@gmail.com>
2026-03-18 01:14:03 +0900

    [Java] 백준 - 다리놓기

---
+++ src/BaekJoonAlgorithm/Re_silver/level5/다리놓기.java
@@ -0,0 +1,18 @@
+package BaekJoonAlgorithm.Re_silver.level5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 다리놓기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+        }
+    }
+}
