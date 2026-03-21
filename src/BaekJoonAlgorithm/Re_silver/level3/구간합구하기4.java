2ee9764cd84579b00db62988e49625991de3bb11
CHISANW <keuye06380618@gmail.com>
2026-03-22 00:58:29 +0900

    [Java] 백준 - 구간합구하기4

---
+++ src/BaekJoonAlgorithm/Re_silver/level3/구간합구하기4.java
@@ -0,0 +1,38 @@
+package BaekJoonAlgorithm.Re_silver.level3;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 구간합구하기4 {
+
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[] arr = new int[n+1];
+    st= new StringTokenizer(br.readLine());
+    for(int i =1; i<=n;i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int[] sum = new int[n+1];
+    for(int i =1; i<=n;i++){
+      sum[i] += sum[i-1] + arr[i];
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for(int i =0; i<m;i++){
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      sb.append(sum[b]-sum[a-1]).append("\n");
+    }
+
+    System.out.println(sb);
+  }
+
+}
