a790e191b5327b9ba3223e7073ddf294e896a864
MinWoo <141993775+CHISANW@users.noreply.github.com>
2026-06-08 22:28:04 +0900

    [Passed] 비교 연산 (48ms, 16MB)

---
+++ tree/master/src/codeTree/trail0/비교 연산/comparison-operator.py
@@ -0,0 +1,11 @@
+true = 1
+false = 0
+
+a , b = map(int, input().split())
+
+print(true if a>=b else false)
+print(true if a> b else false)
+print(true if a<=b else false)
+print(true if a<b else false)
+print(true if a==b else false)
+print(true if a!=b else false)
